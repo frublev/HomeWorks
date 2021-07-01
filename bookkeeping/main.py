3cfc30dc6607af4568ee30ea936adbcdf0d028e9
frublev <f.rublev@gmail.com>
2021-07-01 13:36:47 +0200

    final version

--- bookkeeping/main.py
+++ bookkeeping/main.py
@@ -1,6 +1,9 @@
 from bookkeeping.application.db.people import get_employees
 from bookkeeping.application.salary import calculate_salary
+from datetime import datetime
+
 
 if __name__ == '__main__':
+    print(datetime.now().date())
     get_employees()
     calculate_salary()
